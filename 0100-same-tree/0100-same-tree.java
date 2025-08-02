01105d5558bf1902b512b5593168cf3fc8accb7e
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-08-02 21:24:22 +0530

    Time: 0 ms (100%), Space: 41.1 MB (69.35%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -7,11 +7,11 @@ class Solution {
         if((p!=null && q==null)||(p ==null && q!=null)) return false;
         if(p.val !=q.val) return false;
  
-        if((p.left !=null && q.left==null)||(p.left ==null && q.left!=null)) return false;
-        if((p.right !=null && q.right==null)||(p.right ==null && q.right!=null)) return false;
-        if(((p.left!=null && q.left!=null) && (q.left.val!=q.left.val ))||((p.right!=null && q.right!=null) && (q.right.val!=q.right.val ))){
-           return false;
-        }
+        // if((p.left !=null && q.left==null)||(p.left ==null && q.left!=null)) return false;
+        // if((p.right !=null && q.right==null)||(p.right ==null && q.right!=null)) return false;
+        // if(((p.left!=null && q.left!=null) && (q.left.val!=q.left.val ))||((p.right!=null && q.right!=null) && (q.right.val!=q.right.val ))){
+        //    return false;
+        // }
 
         return (same(p.left,q.left) && same(p.right,q.right));
     }
