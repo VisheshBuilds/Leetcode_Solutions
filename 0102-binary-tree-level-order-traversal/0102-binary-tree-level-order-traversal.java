af41d05fcb0a0681d636dad4ae61038ec28ecc38
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-08-04 11:00:44 +0530

    Time: 2 ms (13.07%), Space: 45 MB (74.87%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,16 +1,58 @@
 
 class Solution {
     public int n;
+
+    public class Pair{
+        TreeNode node;
+        int level;
+        Pair(TreeNode node,int level){
+            this.node=node;
+            this.level=level;
+        }
+    }
+
     public List<List<Integer>> levelOrder(TreeNode root) {
+                //METHOD 1(RECURSION)
+
+        // List<List<Integer>> ans=new ArrayList<>();
+        // n=level(root);
+        // for(int i=0;i<n;i++){
+        //     List<Integer> arr=new ArrayList<>();
+        //    nthlevel(root,0,i,arr);
+        //    ans.add(arr);
+        // }
+        // return ans;
+
+
+              //METHOD 2(QUEUE)
+        
         List<List<Integer>> ans=new ArrayList<>();
         n=level(root);
         for(int i=0;i<n;i++){
             List<Integer> arr=new ArrayList<>();
-           nthlevel(root,0,i,arr);
-           ans.add(arr);
+            ans.add(arr);
         }
+        levelorderq(root,ans);
         return ans;
     }
+
+
+    private  void levelorderq(TreeNode root,List<List<Integer>> ans){
+        Queue<Pair> q2=new LinkedList<>();
+        if(root!=null) q2.add(new Pair(root,0));
+        while(q2.size()>0){
+            Pair front=q2.remove();
+            TreeNode temp=front.node;
+            int lvl=front.level;
+
+            ans.get(lvl).add(temp.val);
+
+            if(temp.left!=null) q2.add(new Pair(temp.left,lvl+1));
+            if(temp.right!=null) q2.add(new Pair(temp.right,lvl+1));
+        }
+    }
+
+
     public void nthlevel(TreeNode root,int level,int req,List<Integer> arr){
        if(root==null) return;
        if(level>req) return;
