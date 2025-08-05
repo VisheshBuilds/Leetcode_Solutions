9ce72b9196c8627c8a2aa39a33b6433610251401
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-08-05 21:08:47 +0530

    Time: 1 ms (63.69%), Space: 42.3 MB (39.23%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,45 @@
+class Solution {
+    public class Pair{
+        TreeNode node;
+        int level;
+        Pair(TreeNode node,int level){
+            this.node=node;
+            this.level=level;
+        }
+    }
+    public int level(TreeNode root){
+        if(root==null) return 0;
+        return 1+Math.max(level(root.left),level(root.right));
+    }
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            //METHOD 1: level order using q
+        int n=level(root);
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            List<Integer> arr=new ArrayList<>();
+            ans.add(arr);
+        }    
+        levelorder(root,ans);
+        for(int i=1;i<n;i=i+2){
+            Collections.reverse(ans.get(i));
+        }
+
+        return ans;
+
+             //METHOD 2: recursion(nth level)
+        // int n=level(root);     
+    }
+    public void levelorder(TreeNode root,List<List<Integer>> ans){
+        Queue<Pair> q=new LinkedList<>();
+        if(root!=null) q.add(new Pair(root,0));
+        while(q.size()>0){
+        Pair front=q.remove();
+        TreeNode temp=front.node;
+        int lvl=front.level;
+        ans.get(lvl).add(temp.val);
+
+        if(temp.left!=null) q.add(new Pair(temp.left,lvl+1));
+        if(temp.right!=null) q.add(new Pair(temp.right,lvl+1));
+        }
+    }
+}
