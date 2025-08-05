53399403306d0b929cadbc4aaf3d895400287f64
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-08-05 23:02:45 +0530

    Time: 1 ms (7.16%), Space: 41.7 MB (66.34%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -1,25 +1,24 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ans=new ArrayList<>();
-        postorder(root,ans);
-        return ans;
+           //METHOD 1(RECURSIVE)
+        // List<Integer> ans=new ArrayList<>();
+        // postorder(root,ans);
+        // return ans;
+
+            //METHOD 2(USING STACK)
+        List<Integer> list=new ArrayList<>();
+        Stack<TreeNode> st=new Stack<>();
+        if(root!=null) st.push(root);
+        while(st.size()>0){
+            TreeNode temp=st.pop();
+            list.add(temp.val);
+            if(temp.left!=null) st.push(temp.left);
+            if(temp.right!=null) st.push(temp.right);
+        }
+        Collections.reverse(list);
+        return list;
     }
-        private static void postorder(TreeNode root,List<Integer> ans){
+    private static void postorder(TreeNode root,List<Integer> ans){
         if(root==null) return;  //base case
         postorder(root.left,ans);
         postorder(root.right,ans);
