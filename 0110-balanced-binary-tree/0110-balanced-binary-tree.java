d2f130e1aab9d283ec4b388634a7c4ad34b4660e
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-08-25 19:43:04 +0530

    Time: 0 ms (100%), Space: 44.3 MB (72.47%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -4,8 +4,16 @@ class Solution {
         return 1+Math.max(level(root.left),level(root.right));
     }
     public boolean isBalanced(TreeNode root) {
+                //METHOD 1(SELF)
+
+        // if(root==null) return true;
+        // return preorder(root);
+
+                //METHOD 2(SIR)
         if(root==null) return true;
-        return preorder(root);
+        if(Math.abs(level(root.left)-level(root.right))>1) return false;
+        return isBalanced(root.left) && isBalanced(root.right);
+
     }
     public boolean preorder(TreeNode root){
         if(root==null) return true;
