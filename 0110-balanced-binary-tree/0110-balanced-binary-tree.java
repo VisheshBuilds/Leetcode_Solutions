9a9cc18bedcfa8a2155b3e9ba895759c8ab85a9e
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-08-25 19:52:40 +0530

    Time: 0 ms (100%), Space: 44.2 MB (86.45%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,19 +1,8 @@
 class Solution {
+    static boolean ans;
     public int level(TreeNode root){
         if(root==null) return 0;
         return 1+Math.max(level(root.left),level(root.right));
-    }
-    public boolean isBalanced(TreeNode root) {
-                //METHOD 1(SELF)
-
-        // if(root==null) return true;
-        // return preorder(root);
-
-                //METHOD 2(SIR)
-        if(root==null) return true;
-        if(Math.abs(level(root.left)-level(root.right))>1) return false;
-        return isBalanced(root.left) && isBalanced(root.right);
-
     }
     public boolean preorder(TreeNode root){
         if(root==null) return true;
@@ -22,4 +11,31 @@ class Solution {
         if(!preorder(root.right)) return false;
         return true;
     }
+    public int levels(TreeNode root){
+        if(root==null) return 0;
+        int lst=levels(root.left);
+        int rst=levels(root.right);
+        int diff=Math.abs(lst-rst);
+        if(diff>1) ans=false;
+        return 1+Math.max(lst,rst);
+    }
+    public boolean isBalanced(TreeNode root) {
+                //METHOD 1(SELF)
+
+        // if(root==null) return true;
+        // return preorder(root);
+
+                //METHOD 2(SIR)
+        // if(root==null) return true;
+        // if(Math.abs(level(root.left)-level(root.right))>1) return false;
+        // return isBalanced(root.left) && isBalanced(root.right);
+               
+               //METHOD 3(SIR)
+         
+         ans=true;
+         levels(root);
+         return ans;
+
+    }
+  
 }
