11c6d17623e4163cdf8a12fc7b2bc7396e00ae38
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-09-09 21:39:55 +0530

    Time: 25 ms (67.6%), Space: 47.9 MB (98.88%) - LeetHub

--- 3999-minimum-operations-to-transform-string/3999-minimum-operations-to-transform-string.java
+++ 3999-minimum-operations-to-transform-string/3999-minimum-operations-to-transform-string.java
@@ -1,17 +1,29 @@
 class Solution {
     public int minOperations(String s) {
-        int[] alpha=new int[26];
+              //METHOD 1(SELF)
+        // int[] alpha=new int[26];
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     alpha[ch-'a']++;
+        // }
+        // int ans=0;
+        // for(int i=1;i<alpha.length;i++){
+        //    if(alpha[i]!=0){
+        //       ans=26-i;
+        //       break;
+        //    } 
+        // }
+        // return ans;
+
+               //METHOD 2(SELF)
+
+        int ans=0;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            alpha[ch-'a']++;
-        }
-        int ans=0;
-        for(int i=1;i<alpha.length;i++){
-           if(alpha[i]!=0){
-              ans=26-i;
-              break;
-           } 
-        }
+            if(ch!='a'){
+            ans=Math.max(ans,26-(ch-'a'));
+            }
+        }       
         return ans;
-    }
+    } 
 }
