994b72267c21da42fde56e2eff3ae929b5aa31a3
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-09-10 23:56:56 +0530

    Time: 0 ms (100%), Space: 40.7 MB (52.98%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canMeasureWater(int x, int y, int target) {
+        if(x+y <target) return false;
+        int k=gcd(x,y);
+        if(target%k==0) return true;
+        return false;
+    }
+    public int gcd(int a,int b){
+        if(b%a==0) return a;
+        return gcd(b%a,a);
+    }
+}
