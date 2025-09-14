4236a82caf3293ceddf85cc99eb49a5a950784a9
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-09-14 17:32:42 +0530

    Time: 2 ms (10.94%), Space: 45.2 MB (5.67%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,5 +1,5 @@
 class Solution {
-       public boolean flag;
+    public boolean flag;
     public long max(TreeNode root){
         if(root==null) return Long.MIN_VALUE;
         long a=root.val;
@@ -16,38 +16,24 @@ class Solution {
     }
     public boolean isValidBST(TreeNode root) {
                     //METHOD 1 (BRUTE FORCE SELF)
-        // List<Integer> ans=new ArrayList<>();
-        // inorder(root,ans);
-        // boolean valid=true;
-        // for(int i=1;i<ans.size();i++){
-        //   if(ans.get(i-1)>= ans.get(i)) valid=false;
-        // }
-        // return valid;
+        List<Integer> ans=new ArrayList<>();
+        inorder(root,ans);
+        boolean valid=true;
+        for(int i=1;i<ans.size();i++){
+          if(ans.get(i-1)>= ans.get(i)) valid=false;
+        }
+        return valid;
 
                     //METHOD 2(SIR)
-        flag=true;
-        max(root);
-        min(root);
-        return flag;    
+        // flag=true;
+        // max(root);
+        // min(root);
+        // return flag;    
     }
-
-    public boolean isvalid(TreeNode root){
-        if(root==null) return true;
-        if(root.left!=null && root.val <= root.left.val){
-           return false;
-        }
-        if(root.right!=null && root.val >= root.right.val){
-           return false;
-        }
-        return true;
-        // return isvalid(root.left) && isvalid(root.right);
-    }
-
     public void inorder(TreeNode root,List<Integer> arr){
         if(root==null) return ;
         inorder(root.left,arr);
         arr.add(root.val);
         inorder(root.right,arr);
-        return;
     }
 }
