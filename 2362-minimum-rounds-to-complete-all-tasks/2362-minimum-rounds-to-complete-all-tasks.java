05c9517740cb6027d2c14acae171cf208378ca93
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-09-17 00:25:15 +0530

    Time: 30 ms (88.58%), Space: 56.1 MB (96.9%) - LeetHub

--- 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.java
+++ 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.java
@@ -1,19 +1,31 @@
 class Solution {
     public int minimumRounds(int[] tasks) {
+             //METHOD 1(SELF)
+        // Map<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<tasks.length;i++){
+        //     map.put(tasks[i],map.getOrDefault(tasks[i],0)+1);
+        // }
+        // int ans=0;
+        // for(int key:map.keySet()){
+        //     int value=map.get(key);
+        //     if(value==1) return -1;
+        //     ans +=helper(value,0);
+        // }
+        // return ans;
+
+             //METHOD 2(CHATGPT)
         Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<tasks.length;i++){
-            map.put(tasks[i],map.getOrDefault(tasks[i],0)+1);
+        for(int task:tasks){
+            map.put(task,map.getOrDefault(task,0)+1);
         }
         int ans=0;
-        for(int key:map.keySet()){
-            int value=map.get(key);
-            if(value==1) return -1;
-            ans +=helper(value,0);
+        for(int val:map.values()){
+            if(val==1) return -1;
+            ans +=(val+2)/3;
         }
         return ans;
     }
     public int helper(int value,int step){
-        // if(value==2 || value==3) return step+1;
         if(value%3==0) return step+value/3;
         else if(value%3==2) return step+value/3+1;
         return helper(value-2,step+1);
