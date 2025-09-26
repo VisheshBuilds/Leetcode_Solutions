4d5d80246cde59b2b87d7e256026d00bd4780911
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-09-26 18:26:25 +0530

    Time: 0 ms (100%), Space: 42.6 MB (10.46%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,12 +1,34 @@
 class Solution {
     public void flatten(TreeNode root) {
-        //METHOD 1(BASIC)
-        List<TreeNode> list=new ArrayList<>();
-         preorder(root,list);
-         for(int i=1;i<list.size();i++){
-            list.get(i-1).right=list.get(i);
-            list.get(i-1).left=null;
-         }
+                //METHOD 1(BASIC)
+        // List<TreeNode> list=new ArrayList<>();
+        // preorder(root,list);
+        // for(int i=1;i<list.size();i++){
+        //     list.get(i-1).right=list.get(i);
+        //     list.get(i-1).left=null;
+        // }
+
+                //METHOD 2(MORRIS)
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode pred=curr.left;
+                while(pred.right!=null){
+                    pred=pred.right;
+                }
+                if(pred.right==null){
+                    pred.right=curr.right;
+                    TreeNode n=curr.left;
+                    curr.right=curr.left;
+                    curr.left=null;
+                    curr=n;
+                }
+            }
+            else {
+                 curr=curr.right;
+            }
+        }   
+
     }
     public void preorder(TreeNode root,List<TreeNode> list){
         if(root==null) return;
