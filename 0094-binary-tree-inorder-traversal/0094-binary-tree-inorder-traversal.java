0bfe180b1a12e52c831b91a20ce3e6d3513a82d6
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-09-26 12:01:20 +0530

    Time: 0 ms (100%), Space: 41.9 MB (16.15%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -6,22 +6,50 @@ class Solution {
         // return ans;
 
               //METHOD 2(USING STACK)
-        List<Integer> ans=new ArrayList<>();
-        Stack<TreeNode> st=new Stack<>();
-        TreeNode temp=root;
-        while(true){
-            if(temp!=null){
-                st.push(temp);
-                temp=temp.left;
+        // List<Integer> ans=new ArrayList<>();
+        // Stack<TreeNode> st=new Stack<>();
+        // TreeNode temp=root;
+        // while(true){
+        //     if(temp!=null){
+        //         st.push(temp);
+        //         temp=temp.left;
+        //     }
+        //     else{
+        //         if(st.isEmpty()) break;
+        //         TreeNode top=st.pop();
+        //         ans.add(top.val);
+        //         temp=top.right;
+        //     }
+        // }
+        // return ans;
+
+             //METHOD 3(MORRIS TRAVERSAL)
+        List<Integer> ans=new ArrayList<>();     
+        TreeNode curr=new TreeNode();
+        curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode pred=curr.left;
+                while(pred.right!=null && pred.right!=curr){
+                    pred=pred.right;
+                }
+                if(pred.right==null){  //link
+                    pred.right=curr;
+                    curr=curr.left;
+                }
+                else{       //unlink
+                    ans.add(curr.val);
+                    curr=curr.right;
+                    pred.right=null;
+                }
             }
-            else{
-                if(st.isEmpty()) break;
-                TreeNode top=st.pop();
-                ans.add(top.val);
-                temp=top.right;
+            else {
+                ans.add(curr.val);
+                curr=curr.right;
             }
         }
         return ans;
+
 
     }
     private static void inorder(TreeNode root,List<Integer> ans){
