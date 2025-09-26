74df4e7a26aa80302c63b64980b2a627cc3697ab
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-09-26 12:10:07 +0530

    Time: 1 ms (17.66%), Space: 43.3 MB (79.12%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -16,19 +16,51 @@ class Solution {
     }
     public boolean isValidBST(TreeNode root) {
                     //METHOD 1 (BRUTE FORCE SELF)
-        List<Integer> ans=new ArrayList<>();
-        inorder(root,ans);
-        boolean valid=true;
-        for(int i=1;i<ans.size();i++){
-          if(ans.get(i-1)>= ans.get(i)) valid=false;
-        }
-        return valid;
+        // List<Integer> ans=new ArrayList<>();
+        // inorder(root,ans);
+        // boolean valid=true;
+        // for(int i=1;i<ans.size();i++){
+        //   if(ans.get(i-1)>= ans.get(i)) valid=false;
+        // }
+        // return valid;
 
                     //METHOD 2(SIR)
         // flag=true;
         // max(root);
         // min(root);
-        // return flag;    
+        // return flag;  
+
+                   //METHOD 3(MORRIS TRAVERSAL)
+        TreeNode prev=null;           
+        TreeNode curr=new TreeNode();
+        curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode pred=curr.left;
+                while(pred.right!=null && pred.right!=curr){
+                    pred=pred.right;
+                }
+                if(pred.right==null){  //link
+                    pred.right=curr;
+                    curr=curr.left;
+                }
+                else{       //unlink
+                    // ans.add(curr.val);
+                    if(prev!=null && prev.val>=curr.val) return false;
+                    prev=curr;
+                    curr=curr.right;
+                    pred.right=null;
+                }
+            }
+            else {
+                // ans.add(curr.val);  //visit
+                if(prev!=null && prev.val>=curr.val) return false;
+                prev=curr;
+                curr=curr.right;
+            }
+        } 
+
+        return true;            
     }
     public void inorder(TreeNode root,List<Integer> arr){
         if(root==null) return ;
