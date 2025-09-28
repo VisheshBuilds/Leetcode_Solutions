174cdf1f37221e4399e2a15ca60cb0e104caa986
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-09-28 15:14:33 +0530

    Time: 8 ms (52.54%), Space: 45 MB (84.13%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
@@ -1,24 +1,13 @@
 class Solution {
     public int largestPerimeter(int[] nums) {
-        int ans=0,n=nums.length,i=0,j=1,k=2;
+        int ans=0,n=nums.length,i=n-1,j=n-2,k=n-3;
         Arrays.sort(nums);
-        // for(int i=0;i<n-2;i++){
-        //     for(int j=i+1;j<n-1;j++){
-        //         for(int k=j+1;k<n;k++){
-        //             int sum=nums[i]+nums[j]+nums[k];
-        //             if(isValid(nums[i],nums[j],nums[k]) && ans<sum) ans=sum;
-        //         }
-        //     }
-        // }
-
-        while(k<n){
+        while(k>=0){
             int sum=nums[i]+nums[j]+nums[k];
-            if(isValid(nums[i],nums[j],nums[k]) && ans<sum) ans=sum;
-            i++; j++; k++;
+            if(nums[i]<nums[j]+nums[k]) return sum;
+            i--;j--;k--;
         }
-
-
-        return ans;
+        return 0;
     }
     public boolean isValid(int x,int y,int z){
         if(x+y > z && x+z >y && y+z >x) return true;
