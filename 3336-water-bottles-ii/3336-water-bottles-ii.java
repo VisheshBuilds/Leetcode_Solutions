6eb84fb265a623b6b9b98b5e6b5d67c8f21a0c60
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-10-02 20:18:45 +0530

    Time: 0 ms (100%), Space: 41.1 MB (45.81%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int dB=numBottles,rb=numBottles,n=numExchange,fb=0;
+        while(rb >=n){
+            int rem=rb-n;
+            dB +=1;
+            rb = rem+1;
+            n++;
+        }
+        return dB;
+    }
+}
