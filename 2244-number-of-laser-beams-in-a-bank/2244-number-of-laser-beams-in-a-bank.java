f2cb454102733e4ff1dba8d7f1306e1b263231ac
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-10-27 23:06:17 +0530

    Time: 10 ms (89.6%), Space: 45 MB (76.08%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
@@ -1,11 +1,23 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        int ans=0;
+        //Method 1(self)
+
+        // int ans=0;
+        // for(int i=0;i<bank.length;i++){
+        //     int one=ones(bank[i]);
+        //     int j=i+1;
+        //     while(j<bank.length && ones(bank[j])==0) j++;
+        //     if(j<bank.length) ans +=one*ones(bank[j]);
+        // }
+        // return ans;
+
+        //Method 2 (clue)
+        int ans=0,prevrowcount=0;
         for(int i=0;i<bank.length;i++){
-            int one=ones(bank[i]);
-            int j=i+1;
-            while(j<bank.length && ones(bank[j])==0) j++;
-            if(j<bank.length) ans +=one*ones(bank[j]);
+            int currowcount=ones(bank[i]);
+            if(currowcount==0) continue;
+            ans +=currowcount*prevrowcount;
+            prevrowcount=currowcount;
         }
         return ans;
     }
