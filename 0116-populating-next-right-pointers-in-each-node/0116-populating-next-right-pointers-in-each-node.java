9710fccd38186135ba4f6c4b6f728a7e8df312e5
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-10-30 22:34:50 +0530

    Time: 0 ms (100%), Space: 44.5 MB (46.49%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -29,7 +29,7 @@ class Solution {
     public void helper(Node root){
         if(root==null) return;
         if(root.next !=null) {
-            if(root.next.val<-1000 && root.next.val>1000) root.next=null;
+            if(root.next.val<-1000 || root.next.val>1000) root.next=null;
         }
         Node prev=root.left;
         Node nxt=root.right;
