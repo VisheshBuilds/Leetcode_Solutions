d6cefcc96711adadad58337707613c6a3a6249b9
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-11-16 00:27:29 +0530

    Time: 7 ms (74.24%), Space: 47.1 MB (19.39%) - LeetHub

--- 0835-linked-list-components/0835-linked-list-components.java
+++ 0835-linked-list-components/0835-linked-list-components.java
@@ -1,23 +1,37 @@
-
 class Solution {
     public int numComponents(ListNode head, int[] nums) {
-        int ans=0;
+                //METHOD 1 (SELF)
+
+        // int ans=0;
+        // HashSet<Integer> set=new HashSet<>();
+        // for(int i:nums) set.add(i);
+        // ListNode temp=head;
+        // while(temp!=null){
+        //     while(!set.contains(temp.val)) {
+        //         temp=temp.next;
+        //         if(temp==null) return ans;
+        //     }
+        //     while(temp!=null && set.contains(temp.val)){
+        //         temp=temp.next;
+        //     }
+        //     ans++;
+        //     if(temp!=null) temp=temp.next;
+        // }
+        // return ans;
+
+              //METHOD 2(CHATGPT)
+
         HashSet<Integer> set=new HashSet<>();
-        for(int i:nums){
-            set.add(i);
-        }
-        ListNode temp=head;
-        while(temp!=null){
-            while(!set.contains(temp.val)) {
-                temp=temp.next;
-                if(temp==null) return ans;
+        for(int i:nums) set.add(i);
+        int ans=0;
+        ListNode t=head;
+        while(t!=null){
+            //if node is in nums AND node.next not in nums 
+            if((set.contains(t.val)) && (t.next==null || !set.contains(t.next.val) )){
+                ans++;
             }
-            while(temp!=null && set.contains(temp.val)){
-                temp=temp.next;
-            }
-            ans++;
-            if(temp!=null) temp=temp.next;
-        }
-        return ans;
+            t=t.next;
+        }  
+        return ans;    
     }
 }
