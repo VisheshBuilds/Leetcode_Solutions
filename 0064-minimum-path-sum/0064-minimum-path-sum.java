455c8150841d2bed7b4f116675d146d78b94c57f
VisheshBuilds <csaiml23048@glbitm.ac.in>
2025-11-25 20:16:23 +0530

    Time: 4 ms (61.15%), Space: 50.3 MB (17.32%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minPathSum(int[][] dp) {
+        int m=dp.length;
+        int n=dp[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                else if(i==0) dp[i][j]=dp[i][j]+dp[i][j-1];
+                else if(j==0) dp[i][j]=dp[i][j]+dp[i-1][j];
+                else dp[i][j] +=Math.min(dp[i][j-1],dp[i-1][j]);
+            }
+        } 
+        return dp[m-1][n-1];
+    }
+}
