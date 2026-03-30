0bcb4ac7028d81461a93615406a47f424a0fec03
VisheshBuilds <csaiml23048@glbitm.ac.in>
2026-03-30 15:09:30 +0530

    Time: 1 ms (99.37%) | Memory: 46.8 MB (68.63%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+             //METHOD 1
+        // int n=mat.length,m=mat[0].length;
+        // k=k%m;
+        // // if(k<m) nk=k;
+        // // else nk = m%k;
+        // int[][] ans=new int[n][m];
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         int x;
+        //         if(i%2==0){
+        //             ans[i][j]= mat[i][(j+k)%m];
+        //         }
+        //         else {
+        //              ans[i][j]= mat[i][(j-k+m)%m];
+        //         }
+        //     }
+        // }
+
+        // for(int i=0;i<n;i=i++){
+        //     for(int j=0;j<m;j++){
+        //         if(ans[i][j] != mat[i][j]) return false;
+        //     }
+        // }
+        // return true;
+  
+
+          //METHOD 2
+        int n=mat.length,m=mat[0].length;
+        k=k%m;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int x;
+                if(i%2==0){
+                   x= mat[i][(j+k)%m];
+                }
+                else {
+                    x= mat[i][(j-k+m)%m];
+                }
+                if(mat[i][j]!=x) return false;
+            }
+        }
+
+        return true;
+    }
+}
