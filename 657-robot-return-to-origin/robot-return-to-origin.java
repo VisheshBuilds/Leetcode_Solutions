4ec4b82c8f3f2d7d49041ab505a63cb8002fda59
VisheshBuilds <csaiml23048@glbitm.ac.in>
2026-04-06 10:47:35 +0530

    Time: 6 ms (49.40%) | Memory: 45.4 MB (91.54%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,15 +1,26 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int[] ans=new int[4];
+              //METHOD 1
+        // int[] ans=new int[4];
+        // for(int i=0;i<moves.length();i++){
+        //     char ch= moves.charAt(i);
+        //     if(ch=='U') ans[0]++;
+        //     else if(ch=='D') ans[1]++;
+        //     else if(ch=='L') ans[2]++;
+        //     else ans[3]++;
+        // }
+        // if(ans[0]==ans[1] && ans[2]==ans[3]) return true;
+        // return false;
+
+             //METHOD 2
+        int x=0,y=0;
         for(int i=0;i<moves.length();i++){
             char ch= moves.charAt(i);
-            if(ch=='U') ans[0]++;
-            else if(ch=='D') ans[1]++;
-            else if(ch=='L') ans[2]++;
-            else ans[3]++;
+            if(ch=='U') x++;
+            else if(ch=='D') x--;
+            else if(ch=='L') y++;
+            else y--;
         }
-
-        if(ans[0]==ans[1] && ans[2]==ans[3]) return true;
-        return false;
+        return x==0 && y==0;
     }
 }
