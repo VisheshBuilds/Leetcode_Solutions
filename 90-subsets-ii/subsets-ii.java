002218aba4a680eeea05ea63537607fb3d345bbb
VisheshBuilds <csaiml23048@glbitm.ac.in>
2026-07-05 00:24:15 +0530

    Time: 3 ms (56.74%) | Memory: 44.8 MB (88.92%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -4,7 +4,12 @@ class Solution {
         Arrays.sort(nums);
         boolean[] visited=new boolean[n];
         List<List<Integer>> ans=new ArrayList<>();
-        helper(ans,visited,nums,new ArrayList<>(),0);
+
+         //METHOD 1
+        // helper(ans,visited,nums,new ArrayList<>(),0);
+
+          //METHOD 2
+        helper2(ans,nums,new ArrayList<>(),0);  
         return ans;
     }
     public void helper(List<List<Integer>> ans,boolean[] visited,int[] nums,List<Integer> temp,int s){
@@ -18,6 +23,18 @@ class Solution {
             helper(ans,visited,nums,temp,i+1);
             temp.remove(temp.size()-1);
             visited[i]=false;
+        }
+
+    }
+
+    public void helper2(List<List<Integer>> ans,int[] nums,List<Integer> temp,int s){
+            ans.add(new ArrayList<>(temp));
+
+        for(int i=s;i<nums.length;i++){
+            if(i>s && nums[i-1]==nums[i]) continue;
+            temp.add(nums[i]);
+            helper2(ans,nums,temp,i+1);
+            temp.remove(temp.size()-1);
         }
 
     }
