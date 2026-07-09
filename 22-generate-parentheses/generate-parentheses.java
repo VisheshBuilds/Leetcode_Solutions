c22a9460b3c042616d514e02e0dfd6a7a77a5534
VisheshBuilds <csaiml23048@glbitm.ac.in>
2026-07-09 11:47:22 +0530

    Time: 2 ms (71.33%) | Memory: 45.2 MB (9.01%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans=new ArrayList<>();
+        helper(n,0,0,"",ans);
+        return ans;
+    }
+    public void helper(int n,int l,int r,String s,List<String> ans){
+        if(l==n && r==n){
+            ans.add(s);
+            return;
+        }
+        if(l>n || r>n) return;
+        helper(n,l+1,r,s+'(',ans);
+        if(r<l){
+            helper(n,l,r+1,s+')',ans);
+        }
+    }
+}
