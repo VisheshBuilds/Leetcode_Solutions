26f9303b6c63b567d4fee51d08b33a8a9be4e391
VisheshBuilds <csaiml23048@glbitm.ac.in>
2026-07-11 18:09:40 +0530

    Time: 3 ms (32.53%) | Memory: 48.9 MB (22.74%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -2,16 +2,14 @@ class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> ans=new ArrayList<>();
         HashMap<Character,String> map=new HashMap<>();
-        for(int i=2;i<10;i++){
-            if(i==2) map.put('2',"abc");
-            if(i==3) map.put('3',"def");
-            if(i==4) map.put('4',"ghi");
-            if(i==5) map.put('5',"jkl");
-            if(i==6) map.put('6',"mno");
-            if(i==7) map.put('7',"pqrs");
-            if(i==8) map.put('8',"tuv");
-            if(i==9) map.put('9',"wxyz");
-        }
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
         helper(0,digits,"",ans,map);
         return ans;
     }
@@ -24,7 +22,6 @@ class Solution {
         for(int i=0;i<map.get(digits.charAt(s)).length();i++){
             char ch=map.get(digits.charAt(s)).charAt(i);
             helper(s+1,digits,temp+ch,ans,map);
-
         }
 
     }
