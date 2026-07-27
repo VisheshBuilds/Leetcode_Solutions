19b36d403c030a2675996d1b9ead40ceb64b8a98
VisheshBuilds <csaiml23048@glbitm.ac.in>
2026-07-27 10:02:13 +0530

    Time: 215 ms (67.49%) | Memory: 43 MB (35.37%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    public boolean solve(char[][] board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+
+                    for(char d='1';d<='9';d++){
+                        if(isValid(board,i,j,d)){
+                            board[i][j]=d;
+                            if(solve(board)) return true;
+                        }
+                        board[i][j]='.';
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isValid(char[][] board,int row,int col,char d){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==d) return false;
+            if(board[i][col]==d) return false;
+        }
+
+        int stri=row/3 * 3;
+        int strj=col/3 * 3;
+        for(int k=0;k<3;k++){
+            for(int l=0;l<3;l++){
+                if(board[stri+k][strj+l]==d) return false;
+            }
+        }
+        return true;
+    }
+}
