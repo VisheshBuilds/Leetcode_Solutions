bc4e6527db9f10070cca700f0ca7081617310cf9
VisheshBuilds <visheshkasaudhan12@gmail.com>
2026-07-31 12:54:07 +0530

    Time: 5 ms (28.13%) | Memory: 51.4 MB (86.13%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n=grid.length,m=grid[0].length;
+        int count=0;
+        boolean[][] visit=new boolean[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!visit[i][j] && grid[i][j]=='1'){
+                    bfs(visit,grid,i,j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    class Pair{
+        int row;
+        int col;
+        Pair(int row,int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
+    public void bfs(boolean[][] visit,char[][] grid,int i,int j){
+        int n=visit.length,m=visit[0].length;
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(i,j));
+        visit[i][j]=true;
+        while(!q.isEmpty()){
+            Pair front=q.remove();
+            int row=front.row,col=front.col;
+            //top row-1,col
+            if(row>0 && !visit[row-1][col]&& grid[row-1][col]=='1'){
+                q.add(new Pair(row-1,col));
+                visit[row-1][col]=true;
+            }
+            //bottom row+1,col
+            if(row<n-1 && !visit[row+1][col]&& grid[row+1][col]=='1'){
+                q.add(new Pair(row+1,col));
+                visit[row+1][col]=true;
+            }
+            //left row,col-1
+            if(col>0 && !visit[row][col-1]&& grid[row][col-1]=='1'){
+                q.add(new Pair(row,col-1));
+                visit[row][col-1]=true;
+            }
+            //right row,col+1
+            if(col<m-1 && !visit[row][col+1]&& grid[row][col+1]=='1'){
+                q.add(new Pair(row,col+1));
+                visit[row][col+1]=true;
+            }
+        }
+
+    }
+}
