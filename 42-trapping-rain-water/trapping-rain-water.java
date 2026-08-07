9d71e59db8235b7800aefb83d5fe337d80a6946f
VisheshBuilds <visheshkasaudhan12@gmail.com>
2026-08-07 13:04:33 +0530

    Time: 1 ms (58.87%) | Memory: 48.6 MB (22.46%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,24 +1,42 @@
 class Solution {
     public int trap(int[] height) {
-        int n=height.length,ans=0;
-        int[] leftmax=new int[n];
-        int[] rightmax=new int[n];
-
-        leftmax[0]=0;
-        for(int i=1;i<n;i++){
-            leftmax[i]=Math.max(leftmax[i-1],height[i-1]);
-        }
-
-        rightmax[n-1]=0;
-        for(int i=n-2;i>=0;i--){
-            rightmax[i]=Math.max(rightmax[i+1],height[i+1]);
-        }
-
+        int n=height.length,max=-1;
+        int[] left=new int[n];
+        int[] right=new int[n];
         for(int i=0;i<n;i++){
-            if(Math.min(leftmax[i],rightmax[i])-height[i] > 0){
-                ans +=Math.min(leftmax[i],rightmax[i])-height[i];
+            if(max==-1) {
+                left[i]=-1;
+                max=height[i];
+            }
+            else if(max>height[i]){
+                left[i]=max;
+            }
+            else{
+                left[i]=height[i];
+                max=height[i];
             }
         }
-        return ans;
+        max=-1;
+        for(int i=n-1;i>=0;i--){
+            if(max==-1) {
+                right[i]=-1;
+                max=height[i];
+            }
+            else if(max>height[i]){
+                right[i]=max;
+            }
+            else{
+                right[i]=height[i];
+                max=height[i];
+            }
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            int val=Math.min(left[i],right[i]);
+            if(val-height[i]>0){
+                count +=val-height[i];
+            }
+        }
+        return count;
     }
 }
