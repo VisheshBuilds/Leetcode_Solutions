a2b95770222d6699cbb60c2f35a63873dfa2552a
VisheshBuilds <visheshkasaudhan12@gmail.com>
2026-08-09 10:49:33 +0530

    Time: 5 ms (68.80%) | Memory: 47.3 MB (20.52%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -3,29 +3,26 @@ class Solution {
         Stack<Integer> st=new Stack<>();
         int n=asteroids.length;
         for(int i=0;i<n;i++){
-            int num=asteroids[i];
-            if(st.isEmpty()) st.push(num);
-            else{
-                if(st.peek()>0 && num>0){
-                    st.push(num);
-                    continue;
-                } 
-                while(!st.isEmpty() && st.peek()>0 && num<0){
-                    int top=st.pop();
-                    if(Math.abs(top)==Math.abs(num)){
-                       num=0;
-                       break;
-                    } 
-                    if(Math.abs(top)>Math.abs(num)) num=top;
+            if(asteroids[i]<0){
+                while(!st.isEmpty() && st.peek()>0){
+                    if(Math.abs(asteroids[i]) > st.peek() ){
+                        st.pop();
+                    }
+                    else break;
                 }
-                if(num!=0) st.push(num);
+                if(st.isEmpty()) st.push(asteroids[i]);
+                else if(st.peek()<0) st.push(asteroids[i]);
+                else if(st.peek()==Math.abs(asteroids[i])) st.pop();
+            }
+            else {
+                st.push(asteroids[i]);
             }
         }
         int[] ans=new int[st.size()];
-        int i=st.size()-1;
+        int j=st.size()-1;
         while(!st.isEmpty()){
-            ans[i]=st.pop();
-            i--;
+             ans[j]=st.pop();
+             j--;
         }
         return ans;
     }
