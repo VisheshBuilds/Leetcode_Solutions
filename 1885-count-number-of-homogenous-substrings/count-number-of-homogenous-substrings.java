c36c6635b3a0011058ad38f86446d34fb297669f
VisheshBuilds <visheshkasaudhan12@gmail.com>
2026-08-10 12:59:38 +0530

    Time: 10 ms (77.68%) | Memory: 47.5 MB (68.97%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.java
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.java
@@ -1,12 +1,25 @@
 class Solution {
     public int countHomogenous(String s) {
-        int ans=0,l=0,n=s.length(),mod=1000000007;
-        for(int r=0;r<n;r++){
-            if(s.charAt(l)==s.charAt(r)) ans = (ans + (r-l+1))%mod;
-            else {
-                l=r;
-                ans++;
-            }
+        // M1
+        // int ans=0,l=0,n=s.length(),mod=1000000007;
+        // for(int r=0;r<n;r++){
+        //     if(s.charAt(l)==s.charAt(r)) ans = (ans + (r-l+1))%mod;
+        //     else {
+        //         l=r;
+        //         ans++;
+        //     }
+        // }
+        // return ans;
+
+
+        //M2
+         
+        int ans=0,len=0,mod=1000000007,n=s.length();
+        for(int i=0;i<n;i++){
+            if(i>0 && s.charAt(i)==s.charAt(i-1)) len++;
+            else len=1;
+
+            ans = (ans+len)%mod;
         }
         return ans;
     }
