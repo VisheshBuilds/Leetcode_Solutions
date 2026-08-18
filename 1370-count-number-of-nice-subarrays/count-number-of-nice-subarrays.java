9fd351968077422451555ff04de9bfc79162ae6d
VisheshBuilds <visheshkasaudhan12@gmail.com>
2026-08-18 23:53:20 +0530

    Time: 15 ms (30.97%) | Memory: 59.5 MB (63.19%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,24 +1,19 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int n=nums.length,ans=0;
-        List<Integer> idx=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(nums[i]%2!=0) idx.add(i);
+        return helper(nums,k)-helper(nums,k-1);
+    }
+    public int helper(int[] nums,int k){
+        if(k<0) return 0;
+        int l=0,r=0,n=nums.length,sum=0,count=0;
+        while(r<n){
+            sum +=(nums[r]%2==0)? 0:1;
+            while(sum>k){
+                sum -=(nums[l]%2==0)? 0:1;
+                l++;
+            }
+            count +=r-l+1;
+            r++;
         }
-        
-        if(idx.size()<k) return 0;
-        int i=0,j=k-1;
-        while(j< idx.size()){
-            int a=idx.get(i),b=idx.get(j);
-            if(i==0) a= a-(-1);
-            else a=a-idx.get(i-1);
-            if(j== idx.size()-1) b=n-b;
-            else b=idx.get(j+1)-b;
-
-            ans +=(a*b);
-
-            i++; j++;
-        }
-        return ans;
+        return count;
     }
 }
