f4d5546556af22eec9bfe517103efb6b17b5df0b
VisheshBuilds <visheshkasaudhan12@gmail.com>
2026-08-18 19:34:06 +0530

    Time: 4 ms (91.26%) | Memory: 46.8 MB (88.88%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,29 +1,33 @@
 class Solution {
+    private boolean ans;
     public boolean canFinish(int n, int[][] pre) {
+        ans=true;
         List<List<Integer>> adj=new ArrayList<>();
-        int[] indegree=new int[n];
-        
         for(int i=0;i<n;i++) adj.add(new ArrayList<>());
 
         for(int i=0;i<pre.length;i++){
             int a=pre[i][0],b=pre[i][1];
             adj.get(b).add(a);
-            indegree[a]++;
         }
-        Queue<Integer> q=new LinkedList<>();
+
+        boolean[] visit=new boolean[n];
+        boolean[] path=new boolean[n];
         for(int i=0;i<n;i++){
-            if(indegree[i]==0) q.add(i);
+            if(!visit[i]) dfs(i,adj,visit,path);
         }
-        
-        List<Integer> ans=new ArrayList<>();
-        while(!q.isEmpty()){
-            int front=q.remove();
-            ans.add(front);
-            for(int ele:adj.get(front)){
-                indegree[ele]--;
-                if(indegree[ele]==0) q.add(ele);
+           
+        return ans;
+    }
+    public void dfs(int i,List<List<Integer>> adj,boolean[] visit,boolean[] path){
+        visit[i]=true;
+        path[i]=true;
+        for(int ele:adj.get(i)){
+            if(path[ele]==true){
+                ans=false;
+                return;
             }
+            if(!visit[ele]) dfs(ele,adj,visit,path);
         }
-        return ans.size()==n;
+        path[i]=false;
     }
 }
