493ea6d1449e919064b8f12723b08a57df09f925
VisheshBuilds <visheshkasaudhan12@gmail.com>
2026-08-19 22:06:02 +0530

    Time: 15 ms (44.41%) | Memory: 46 MB (90.57%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,22 +1,17 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int count=0,i=0,j=0,n=s.length();
-        int[] arr=new int[3];
+        int ans=0,i=0,j=0,n=s.length();
+        int[] c=new int[3];
         while(j<n){
             char ch=s.charAt(j);
-            if(ch=='a') arr[0]++;
-            else if(ch=='b') arr[1]++;
-            else arr[2]++;
-            while(arr[0]>0 && arr[1]>0 && arr[2]>0){
-                count +=n-j;
-                ch=s.charAt(i);
-                if(ch=='a') arr[0]--;
-                else if(ch=='b') arr[1]--;
-                else arr[2]--;
+            c[ch-'a']++;
+            while(c[0]>0 && c[1]>0 && c[2]>0){
+                ans +=n-j;
+                c[s.charAt(i)-'a']--;
                 i++;
             }
             j++;
         }
-        return count;
+        return ans;
     }
 }
