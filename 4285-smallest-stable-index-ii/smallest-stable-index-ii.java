83b3fa656e956fcd611fbd83e4e0bd70bd7708c8
VisheshBuilds <visheshkasaudhan12@gmail.com>
2026-09-05 10:29:50 +0530

    Time: 6 ms (45.80%) | Memory: 126.5 MB (87.40%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n=nums.length,ans=-1;
+        int[] ma=new int[n];
+        int[] mi=new int[n];
+
+        ma[0]=nums[0];
+        for(int i=1;i<n;i++){
+           ma[i]=Math.max(ma[i-1],nums[i]);
+        }
+
+        mi[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            mi[i]=Math.min(mi[i+1],nums[i]);
+        }
+
+        for(int i=0;i<n;i++){
+            if(ma[i]-mi[i] <= k){
+               ans=i;
+               break;
+            }
+        }
+
+        return ans;
+    }
+}
