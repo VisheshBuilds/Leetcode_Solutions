f052f1848e7f86d19c032fa51311f5727520fea5
VisheshBuilds <visheshkasaudhan12@gmail.com>
2026-09-06 13:52:48 +0530

    Time: 61 ms (5.20%) | Memory: 137.9 MB (6.38%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -9,8 +9,8 @@ class Solution {
         else return solve1(s,t,i+1,j);
     }
     public int solve2(String s,String t,int i,int j){
-        if(j==t.length()) return 1;
-        if(i==s.length()) return 0;
+        if(j==t.length()) return dp[i][j]=1;
+        if(i==s.length()) return dp[i][j]=0;
         
         if(dp[i][j]!=-1) return dp[i][j];
         if(s.charAt(i)==t.charAt(j)){
