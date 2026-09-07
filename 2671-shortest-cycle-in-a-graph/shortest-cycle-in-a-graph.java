72681ba28d376f707ee640278b972dac39818141
VisheshBuilds <visheshkasaudhan12@gmail.com>
2026-09-08 00:12:13 +0530

    Time: 351 ms (52.71%) | Memory: 48 MB (13.56%) - LeetSync

---
+++ 2671-shortest-cycle-in-a-graph/shortest-cycle-in-a-graph.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int findShortestCycle(int n, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        for(int i=0;i<edges.length;i++){
+            int u=edges[i][0],v=edges[i][1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        int max=Integer.MAX_VALUE;
+
+        for(int src=0;src<n;src++){
+            int[] parent=new int[n];
+            int[] dist=new int[n];
+
+            Arrays.fill(parent,-1);
+            Arrays.fill(dist,-1);
+
+            Queue<Integer> pq=new LinkedList<>();
+            dist[src]=0;
+            pq.offer(src);
+
+            while(!pq.isEmpty()){
+                int top=pq.poll();
+                for(int ele:adj.get(top)){
+                    if(dist[ele]==-1){
+                        dist[ele]=dist[top]+1;
+                        parent[ele]=top;
+                        pq.offer(ele);
+                    }
+                    else if(ele!=parent[top]){
+                        int dis=dist[top]+dist[ele]+1;
+                        max=Math.min(max,dis);
+                    }
+                }
+            }
+            
+        }
+        return max==Integer.MAX_VALUE ? -1:max;
+    }
+}
