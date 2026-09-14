4d7b1e8aa490840e134badb7a0b5b054fb6298e3
VisheshBuilds <visheshkasaudhan12@gmail.com>
2026-09-14 19:15:11 +0530

    Time: 14 ms (69.91%) | Memory: 47.6 MB (66.70%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,35 +1,32 @@
 class Solution {
+    class Pair implements Comparable<Pair>{
+        int key;
+        int val;
+        Pair(int key,int val){
+            this.key=key;
+            this.val=val;
+        }
+        public int compareTo(Pair p){
+            return p.val-this.val;
+        }
+    }
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> map=new HashMap<>();
         int n=nums.length;
-        for(int i=0;i<n;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
+        if(n==k) return nums;
+        for(int ele:nums) map.put(ele,map.getOrDefault(ele,0)+1);
+
         PriorityQueue<Pair> pq=new PriorityQueue<>();
         for(int key:map.keySet()){
             int val=map.get(key);
             pq.add(new Pair(key,val));
-            if(pq.size()>k) pq.remove();
         }
         int[] ans=new int[k];
-        int i=0;
-        while(pq.size()>0){
-            Pair p=pq.poll();
-            ans[i]=p.num;
-            i++;
+        for(int i=0;i<k;i++){
+            Pair top=pq.remove();
+            ans[i]=top.key;
         }
         return ans;
 
-    }
-    public class Pair implements Comparable<Pair>{
-        int num;
-        int freq;
-        Pair(int num,int freq){
-            this.num=num;
-            this.freq=freq;
-        }
-        public int compareTo(Pair p){
-            return this.freq-p.freq;
-        }
     }
 }
